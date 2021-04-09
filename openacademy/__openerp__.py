fde87768dcf1241e4e68d059a0941bd5297e828b
Manuel Carretero <zihark27@gmail.com>
2021-04-09 17:24:04 +0000

    [REF] openacademy: Add data demo

--- openacademy/__openerp__.py
+++ openacademy/__openerp__.py
@@ -28,9 +28,9 @@
         # 'security/ir.model.access.csv',
         #'templates.xml',
     ],
-    # only loaded in demonstration mode
+
     'demo': [
-        #'demo.xml',
+       'demo/openacademy_course_demo.xml',
     ],
     'installable':True,
     'auto_install':False,
